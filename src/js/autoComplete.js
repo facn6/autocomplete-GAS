311eec34976ee5951d647a9f9cd1c3fb0bef71d3
Sam Harris <sam@jam-bc.com>
2019-07-24 12:47:52 +0300

    autocomplete functions and tests working relates #17

--- src/js/autoComplete.js
+++ src/js/autoComplete.js
@@ -1,9 +1,71 @@
-const autoComplete = (testStr, words) => {
-    const str = testStr.toLowerCase();
-    console.log(str);
-    
+const buildCountIndex = (words) => {
+    let obj = {2: 0};
+    obj.end = words.words.length;
 
-    return [];
+    let length = 2;
+    words.words.map((word, ind)=>{
+        if(word.length > length) {
+            length++;
+            obj[length] = ind;
+        }
+    });
+    console.log('------- running Count Index -------');
+    
+    return obj;
 };
 
-module.exports = autoComplete;
+
+const filterWords = (testStr, words, countIndex) => {
+    const str = testStr.toLowerCase();
+
+    let wordLengthLimit = str.length + 6 < Object.keys(countIndex).length + 1 ? str.length + 6 : 'end'
+
+    let list = [];
+    let count = countIndex[str.length + 1];
+    let limit = countIndex[wordLengthLimit];
+
+    do {
+        if (words.words[count].slice(0, str.length) == str) {
+            list.push(words.words[count])
+        }
+        count++;
+    } while (list.length < 5 && count < limit)
+
+    return list;
+};
+
+module.exports = {buildCountIndex, filterWords};
+
+
+// ----- countIndex reference --------
+
+// const countIndex = {
+//     2: 0,
+//     3: 124,
+//     4: 1435,
+//     5: 6963,
+//     6: 19614,
+//     7: 42030,
+//     8: 75322,
+//     9: 115964,
+//     10: 157190,
+//     11: 193063,
+//     12: 221177,
+//     13: 241631,
+//     14: 255608,
+//     15: 264821,
+//     16: 270633,
+//     17: 272577,
+//     18: 273704,
+//     19: 274299,
+//     20: 274628,
+//     21: 274788,
+//     22: 274850,
+//     23: 274880,
+//     24: 274893,
+//     25: 274902,
+//     26: 274904,
+//     27: 274905,
+//     28: 274906,
+//     'end': 274907
+// }
--- test.js
+++ test.js
@@ -2,26 +2,37 @@ const test = require('tape');
 const autoComplete = require('./src/js/autoComplete.js');
 
 const testObj = {
-    "words": ["me","va","zo","za","yu","abs","vet","veg","vee","vaw","vav","sean","seam","vaal","wins","scye", "ought","valoo","balsa","vater","balti"]
+    "words": ["me","va","zo","za","yu","abs","vet","veg","vee","vaw","vav","sean","seam","vaal","wins","scye", "ought","valoo","valsa","vater","valti"]
 }
 const testObjCopy = {
-    "words": ["me","va","zo","za","yu","abs","vet","veg","vee","vaw","vav","sean","seam","vaal","wins","scye", "ought","valoo","balsa","vater","balti"]
+    "words": ["me","va","zo","za","yu","abs","vet","veg","vee","vaw","vav","sean","seam","vaal","wins","scye", "ought","valoo","valsa","vater","valti"]
 }
+
+const countIndex = { '2': 0, '3': 5, '4': 11, '5': 16, end: 21 }
 const testStr = 'va';
 const testStrCopy = 'va';
-const testCap = 'VA'
+const testStrCap = 'VA';
 
-test('Autocomplete works as pure function', (t) => {
-    const actual = autoComplete(testStr, testObj)
-    const actualCap = autoComplete(testCap, testObj)
-    const actual2 = autoComplete(testStr, testObj)
-    const expected = ["va", "vaw", "vav", "vaal", "valoo"]
+test('autoComplete.buildCountIndex works as a pure function', (t) => {
+    const actual = autoComplete.buildCountIndex(testObj)
+    const actual2 = autoComplete.buildCountIndex(testObj)
+    t.deepEquals(actual, countIndex, "gives five results of increasing length");
+    t.deepEquals(testObj, testObjCopy, "does not mutate given wordlist");
+    t.deepEquals(actual2, countIndex, "given same arguments gives same results");
+    t.end();
+})
+
+test('autoComplete.filterWords works as pure function', (t) => {
+    const actual = autoComplete.filterWords(testStr, testObj, countIndex)
+    const actualCap = autoComplete.filterWords(testStrCap, testObj, countIndex)
+    const actual2 = autoComplete.filterWords(testStr, testObj, countIndex)
+    const expected = ["vaw", "vav", "vaal", "valoo", 'valsa']
     t.deepEquals(actual, expected, "gives five results of increasing length");
     t.deepEquals(testObj, testObjCopy, "does not mutate given wordlist");
     t.deepEquals(testStr, testStrCopy, "does not mutate given string");
     t.deepEquals(actual2, expected, "given same arguments gives same results");
     t.deepEquals(actualCap, expected, "Input string is case insensitive");
-    t.end()
+    t.end();
 })
   
 
