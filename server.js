515db5b9579f64e485411fc266646ac9efb04312
Ahdab Serhan <ms.ahdab@gmail.com>
2019-07-25 12:40:43 +0300

    added a callback to the server.listen method

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const http = require('http');
 const router = require('./src/js/router.js');
-const words = require('./src/data/words.json');
-const autoComplete = require('./src/js/autoComplete');
+
 
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 4000;
 
 const server = http.createServer(router);
 
-server.listen(port);
+server.listen(port,function(){
+    console.log('server running on: http://' + host + ':' + port);
 
-console.log('server running on: http://' + host + ':' + port);
+});
+
 
 module.exports = server;
